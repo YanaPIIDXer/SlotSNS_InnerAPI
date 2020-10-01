d7f656783a6203830b26bbb102dcf9a75a833c21
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-10-02 08:52:15 +0900

    簡単なテストを作成。

--- spec/controllers/register_controller_spec.rb
+++ spec/controllers/register_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe RegisterController, type: :controller do
-
+    describe 'Register API' do
+        # ステータスコードが正常か？
+        it "StatusCode 200" do
+            get '/register'
+            expect(response.status).to eq(200)
+        end
+    end
 end

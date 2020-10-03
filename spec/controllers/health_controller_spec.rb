6279d9eda6822ccf07d817d2a3fa04c97a7f99ee
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-10-03 12:07:28 +0900

    元に戻した。

--- spec/controllers/health_controller_spec.rb
+++ spec/controllers/health_controller_spec.rb
@@ -9,9 +9,5 @@ RSpec.describe HealthController, type: :controller do
       json = JSON.parse(response.body)
       expect(json['condition']).to eq('ok')
     end
-
-    it 'fuck' do
-      expect(false).to eq(true)
-    end
   end
 end

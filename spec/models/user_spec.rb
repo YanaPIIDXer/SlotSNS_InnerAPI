4457a1e23d28cf678e48f75d82ab712dcf3969cc
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-10-03 10:27:31 +0900

    テストケースを記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Register' do
+    name = 'Name'
+    password = 'Password'
+    email = 'example@example.com'
+    user = User.new(name: name, password: password, email: email)
+    result = user.save!
+    expect(result).to be_truthy
+  end
+
+  it 'zero length string is invalid' do
+    user = User.new(name: '', password: '', email: '')
+    expect(user.valid?).to be_falsy
+  end
+
+  it 'null is invalid' do
+    user = User.new(name: nil, password: nil, email: nil)
+    expect(user.valid?).to be_falsy
+  end
+
+  it 'email is unique' do
+    email = 'example@example.com'
+    User.create(name: 'Name', password: 'Password', email: email)
+    user = User.new(name: 'Name', password: 'Password', email: email)
+    expect(user.valid?).to be_falsy
+  end
 end

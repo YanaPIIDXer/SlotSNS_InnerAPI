ee63e02e9440a72d534962986e6fcf35e9de060d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-10-03 13:26:51 +0900

    ユーザ用Controller追加。

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'user/index'
+  post 'user/create'
+  post 'user/update'
+  post 'user/delete'
   get 'session/index'
   post 'session/create'
   post 'session/delete'
-  root :to => 'health#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post "register" => "register#index"
+  root :to => 'health#index'
 end

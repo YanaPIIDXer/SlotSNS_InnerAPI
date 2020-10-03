21b688ff2c40692cd07c03389ff294b519782c71
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-10-03 15:49:34 +0900

    各種テストケース実装。

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+    it 'Create' do
+        post = Post.new(title: "Title", body: "body")
+        result = post.valid? && post.save!
+        expect(result).to be_truthy
+    end
+
+    it 'Zero length string is invalid' do
+        post = Post.new(title: "", body: "")
+        result = post.valid?
+        expect(result).to be_falsy
+    end
+    
+    it 'null is invalid' do
+        post = Post.new(title: nil, body: nil)
+        result = post.valid?
+        expect(result).to be_falsy
+    end
 end

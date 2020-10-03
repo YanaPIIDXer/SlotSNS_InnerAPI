cd4911e58cd489b83eaeb55545ea05acddc7eaca
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-10-03 11:02:21 +0900

    ヘルスチェック用のControllerを追加。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'health/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post "register" => "register#index"
 end

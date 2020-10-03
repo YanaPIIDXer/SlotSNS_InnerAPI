a934a1ccd13926d34bcec840b52b257d212f32fd
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-10-03 12:43:52 +0900

    とりあえずセッションのチェック部分だけ先に記述。

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,10 +1,28 @@
 class SessionController < ApplicationController
   def index
+    result = (session[:id] != nil)
+    render json: { 'result' => result }
   end
 
   def create
+    response = { 'result' => false }
+    if session[:id] != nil
+      response['result'] = true
+      render json: response
+      return
+    end
+
+    render json: response
   end
 
   def delete
+    response = { 'result' => false }
+    if session[:id] == nil
+      response['result'] = true
+      render json: response
+      return
+    end
+
+    render json: response
   end
 end

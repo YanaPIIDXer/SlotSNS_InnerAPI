9328c34623eb4529e9ef86412ad10a85345b85db
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-10-03 13:31:48 +0900

    とりあえず各アクションがレスポンスを返すようにした。

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,13 +1,21 @@
 class UserController < ApplicationController
   def index
+    response = { 'result' => false }
+    render json: response
   end
 
   def create
+    response = { 'result' => false }
+    render json: response
   end
 
   def update
+    response = { 'result' => false }
+    render json: response
   end
 
   def delete
+    response = { 'result' => false }
+    render json: response
   end
 end

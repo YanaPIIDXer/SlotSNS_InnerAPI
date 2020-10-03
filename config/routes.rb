afd6f050cc7e84090dd2d378ad1321367433d753
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-10-03 13:28:00 +0900

    RegisterControllerをリストラ。（UserControllerに置き換えるので）

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,5 @@ Rails.application.routes.draw do
   get 'session/index'
   post 'session/create'
   post 'session/delete'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post "register" => "register#index"
   root :to => 'health#index'
 end

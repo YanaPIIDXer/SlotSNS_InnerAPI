f8d1700d6e36107e300ea1ca1b9f14a23752a775
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-10-03 12:46:58 +0900

    各テストケースでパラメータを渡すようにした。

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -4,21 +4,24 @@ RSpec.describe SessionController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
-      get :index
+      params = { email: "example@example.com", password: "Password" }
+      get :index, params: params
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "POST #create" do
     it "returns http success" do
-      post :create
+      params = { email: "example@example.com", password: "Password" }
+      post :create, params: params
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #delete" do
+  describe "POST #delete" do
     it "returns http success" do
-      post :delete
+      params = { email: "example@example.com", password: "Password" }
+      post :delete, params: params
       expect(response).to have_http_status(:success)
     end
   end

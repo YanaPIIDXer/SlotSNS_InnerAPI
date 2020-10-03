5c5a540fe9b85fea2ed6f4507baecf07468b107b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-10-03 11:06:30 +0900

    ルートに対してヘルスチェックを振った。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'health/index'
+  root :to => 'health#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post "register" => "register#index"
 end
